80d06c3df35dd11a2986bea6b512c0f35c90f103
meytal benchimol <meytal09@gmail.com>
2022-04-05 14:53:14 +0300

    add try & catch to main

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,16 @@
 
 #include <iostream>
 
-int main()
-{
+int main() try {
     SetCalculator(std::cin, std::cout).run();
+}
+
+catch (std::exception& e) {
+    std::cerr << "Exception: " << e.what() << '\n';
+    return EXIT_FAILURE;
+}
+
+catch (...) {
+    std::cerr << "Unknown exception\n";
+    return EXIT_FAILURE;
 }
